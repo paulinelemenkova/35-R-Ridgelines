2bdf145b8f107b679317b89b58b53901182b6b7a
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-10-25 19:58:19 +0300

    Add files via upload

--- 05_Ridgeline_25102025.R
+++ 05_Ridgeline_25102025.R
@@ -1,41 +1,30 @@
 # Library
+library(ggridges)
 library(tidyverse)
 library(ggplot2)
-library(RColorBrewer)
 library(dplyr)
+library(tidyr)
 library(forcats)
+library(RColorBrewer)
+library(viridis)
+library(viridisLite)
 
 df <- read_csv("FAOSTAT_Prices.csv")
 head(df)
 
-
-
-# library
-library(ggridges)
-library(ggplot2)
- 
-# Diamonds dataset is provided by R natively
-#head(diamonds)
- 
-# basic example
+# 1
 ggplot(df, aes(x = Year, y = Item, fill = Item)) +
   geom_density_ridges() +
   theme_ridges() +
   theme(legend.position = "none")
 
-
-
-# to extend a number of colors in RColorBrewer fixed palette
-nb.cols <- length(unique(df$Item))
-my_custom_colors <- colorRampPalette(brewer.pal(9, "Set1"))(nb.cols)
-
-p<- ggplot(data = df, aes(x = Value, y = Item, fill = Item)) +
-    geom_bar(stat="identity", alpha=1.0, width=.4) +
-    labs(title = "Economic output of agriculture sector in India: 2000-2023", x = "Gross Production Value (in current US $)", y = "Products") +
-    scale_x_continuous(labels = function(x) format(x, scientific = F, digits = 1)) +
-#   scale_fill_brewer(palette = "Set1") +
-    scale_fill_manual(values = my_custom_colors) +
-#    coord_flip() +
-#    xlab("") +
-    theme_grey()
-p
+# 2
+ggplot(df, aes(x = Year, y = Item, fill = after_stat(x))) +
+  geom_density_ridges_gradient(scale = 1.0, rel_min_height = 0.01) +
+  scale_fill_viridis(option = "C") +
+  labs(title = 'Producer prices in agriculture of India: 2000-2023') +
+    theme(
+      legend.position="none",
+      panel.spacing = unit(0.1, "lines"),
+      strip.text.x = element_text(size = 8)
+    )
